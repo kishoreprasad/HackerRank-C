c22c0dcfa148d44e1780d3d59b77eab9bf18499e
Kishore Prasad <kishoreprasad@Kishores-Air.acttv.in>
2019-03-25 00:59:56 +0530

    Initial Code

---
+++ Divisible_Sum_Pairs.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int n,k;
+int divisibleSumPairs(int ar[])
+{
+  int count=0;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+    if(ar[i]<ar[j] && ar[i]+ar[j]%k==0)
+      count++;
+    }
+  }
+  return count;
+}
+main()
+{
+  scanf("%d %d",n,k);
+  int ar[n];
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",ar[i]);
+  }
+  printf("%d",divisibleSumPairs);
+}
