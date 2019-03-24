9b7946d84bac39f8d930c63c5a47bab0818624b8
kpkishore <kpkishore@live.com>
2019-03-24 16:47:24 +0530

    Initial File

--- Breaking_the_records.c
+++ Breaking_the_records.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
+int n;
+breakingRecords(int scores[])
+{
+  int highest_score=scores(0),lowest_score=scores(0),high=0,low=0;
+  for(int i=0;i<n;i++)
+  {
+    if(scores[i]>highest_score)
+    {
+      scores[i]=highest_score;
+      high++;
+    }
+  }
+  for(int i=0;i<n;i++)
+  {
+    if(scores[i]<lowest_score)
+    {
+      scores[i]=lowest_score;
+      low++;
+    }
+  }
+  printf("%d %d",high,low);
+  return 0;
+}
 int main()
 {
-
+  scanf("%d",&n);
+  int scores[n];
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&scores[i]);
+  }
+  breakingRecords(scores);
+  return 0;
 }
